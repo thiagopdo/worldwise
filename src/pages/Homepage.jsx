0e086205511192163600aea09fc906e276f5025a
thiagopdo <thiago.falconer@gmail.com>
2023-12-21 02:12:41 -0300

    added css modules for learning purposes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>WorldWise</h1>
 
-      <Link to="/pricing">PRicing</Link>
+      <Link to="/app">Go to the App</Link>
     </div>
   );
 }
