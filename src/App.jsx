1efbf555c788166585f884078d7a8622d8658e88
thiagopdo <thiago.falconer@gmail.com>
2023-12-21 01:26:59 -0300

    added navbar component and added to the pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
+    
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,5 +1,12 @@
+import PageNav from "../components/PageNav";
+
 function Pricing() {
-  return <div>Price</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Price</h1>
+    </div>
+  );
 }
 
 export default Pricing;
