99bbd77cc02f10564605b41571365d73ac5b32eb
thiagopdo <thiago.falconer@gmail.com>
2023-12-28 02:05:50 -0300

    added the list of cities and message comp in chang of empty array

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,9 +1,24 @@
+/* eslint-disable react/prop-types */
+import Spinner from "./Spinner";
+import Message from "./Message";
 import styles from "./CityList.module.css";
+import CityItem from "./CityItem";
 
-function CityList() {
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
   return (
     <div>
-      <ul className={styles.CityList}>CIty</ul>
+      <ul className={styles.cityList}>
+        {cities.map((city) => (
+          <CityItem city={city} key={city.id} />
+        ))}
+      </ul>
     </div>
   );
 }
