26fe6e88dc021b472c919793c01c684da1ee75d4
thiagopdo <thiago.falconer@gmail.com>
2023-12-28 01:26:36 -0300

    added city list comp and jsonserver to fetch fake api

---
+++ src/components/CityList.jsx
@@ -0,0 +1,11 @@
+import styles from "./CityList.module.css";
+
+function CityList() {
+  return (
+    <div>
+      <ul className={styles.CityList}>CIty</ul>
+    </div>
+  );
+}
+
+export default CityList;
