a603ea9d3733d0328a43aaff4ef41cab0431495d
thiagopdo <thiago.falconer@gmail.com>
2024-01-14 00:16:07 -0300

    finished cityview and created BackButton component

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
-
-/* eslint-disable react/prop-types */
-// eslint-disable-next-line no-unused-vars
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -12,12 +10,17 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
+  const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
-
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
